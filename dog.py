66e7561a756dfc48d2953a345ec10688eec0f349
Ashraj Grewal <ashraj.grewal10@gmail.com>
2020-11-23 00:54:22 -0800

    Update dog.py

--- dog.py
+++ dog.py
@@ -1,4 +1,8 @@
+# dog.py
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, breed):
         self.name = name
+        self.breed = breed
         print("dog initialized!")
+    def bark(self):
+        print("Woof!")
